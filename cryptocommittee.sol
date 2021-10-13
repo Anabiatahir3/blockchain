8eba2b19e49feb01e17d29f58c2ce7e270cbdba2
Anabiatahir3 <anabiatahir365@gmail.com>
2021-10-13 22:47:39 +0500

    Money saving cryptocommittee
    In this contract you have to create a committee money saving system, A person can create a committee pool of a certain token and other people can participate in this committee pool by depositing that token a fixed amount decided by the pool creator, then at the end committee intervals of payment, a person among the participants of this pool will get the committee sum amount pre-decided by the pool creator.
    Note: The contract will require you to deposit your testnet tokens if you're willing to participate. Also try to create a security constraint so that a person after receiving their committee money cannot run away, so there must an exiting policy preventing this to happen.

---
+++ cryptocommittee.sol
@@ -0,0 +1,125 @@
+pragma solidity ^0.5.1;
+contract cryptocommittee{
+    address payable owner;
+    mapping (address => uint) public balances;
+    
+    struct Token {
+        address payable owner;
+        uint amount;
+        bool sold;
+    }
+    
+    Token[5] public Committee;
+    
+    
+     event TokenOwnerChanged(
+        uint index
+    );
+    
+    event TokenBoughtfromCommittee(
+        uint index,
+        uint amount,
+        bool sold
+    );
+    
+    event CommitteeAwarded(
+        uint index,
+        uint amount,
+        address winner
+    );
+    
+    constructor() public {
+        owner = msg.sender;
+        
+        
+        Committee[0].amount = 400;
+        Committee[0].sold = false;
+        Committee[0].owner = msg.sender;
+        
+        Committee[1].amount = 400;
+        Committee[1].sold = false;
+        Committee[1].owner = msg.sender;
+        
+        Committee[2].amount = 400;
+        Committee[2].sold = false;
+        Committee[2].owner = msg.sender;
+        
+        Committee[3].amount = 400;
+        Committee[3].sold = false;
+        Committee[3].owner = msg.sender;
+        
+        Committee[4].amount = 400;
+        Committee[4].sold = false;
+        Committee[4].owner = msg.sender;
+        
+    }
+    
+function BuyToken(uint index, uint price) public {
+        Token storage token = Committee[index];
+        
+        require(msg.sender != owner);
+        // the owner of contract can not buy tokens
+        
+        if(price == token.amount && token.sold != true)
+        {
+            Committee[index].sold = true;
+            Committee[index].amount = 0;
+            balances[msg.sender]=price;
+            
+            // the balance of each is equal to the price for which they bought tokens
+            
+            emit TokenBoughtfromCommittee(index,price,true);
+        }
+        
+    }
+    
+function CommitteeGiventoOne(uint index) public payable {
+        
+        address winner = Committee[index].owner;
+        bool checker = false;
+        for(int i=0;i<5;i++)
+        {
+            if(Committee[index].sold==true)
+            {
+                checker=true;
+                continue;
+                
+            }
+            else
+            {
+                checker=false;
+                break;
+            }
+        }
+        
+        if (checker==false)
+        {
+            return;
+            // if all the Committee tokens have not been sold then the money can not be awarded to anyone
+        }
+        
+        else
+        {
+        
+        require(msg.sender == owner);
+        // committe winner only assigned by the main contract starter 
+
+        balances[winner] = balances[msg.sender] * 5;
+
+        // all committee money assigned to the winner
+        
+        for(int i=0;i<5;i++){
+        
+        Committee[index].amount = 400;
+        Committee[index].sold = false;
+        Committee[index].owner = msg.sender;
+        }
+        
+    // the committee tokens are refreshed and are up for sale
+    
+        emit CommitteeAwarded(index,balances[winner],winner);
+        }
+    }
+        
+       
+}
